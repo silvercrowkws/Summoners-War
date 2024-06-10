56eb4f450dde57428cceff48ea9a38e9bb37cf76
DESKTOP-8900FC7\Atents <silvercrowkws@naver.com>
2024-06-10 15:07:23 +0900

    룬, 몬스터 정보 스크립터블 오브젝트

---
+++ Assets/Scripts/Monster/MonsterBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MonsterState
+{
+    Idle = 0,
+    Attack,
+    GetHit,
+    Die
+}
+
+public class MonsterBase : MonoBehaviour
+{
+    /// <summary>
+    /// 몬스터의 기본 상태는 Idle
+    /// </summary>
+    MonsterState monsterState = MonsterState.Idle;
+
+    /// <summary>
+    /// 룬 정보(인스펙터에서 할당)
+    /// </summary>
+    public RuneDB runeDB;
+
+    /// <summary>
+    /// 몬스터 정보(인스펙터에서 할당)
+    /// </summary>
+    public MonsterDB monsterDB;
+
+    /// <summary>
+    /// 게임 매니저
+    /// </summary>
+    GameManager gameManager;
+
+    private void Start()
+    {
+        GameManager gameManager = GameManager.Instance;
+
+        //Debug.Log($"룬 체력 : {runeDB.upHP}");
+        //Debug.Log($"기본 체력 : {monsterDB.baseHP}");
+        //Debug.Log($"합산 체력 : {monsterDB.baseHP * runeDB.upHP}");
+    }
+
+
+
+}
