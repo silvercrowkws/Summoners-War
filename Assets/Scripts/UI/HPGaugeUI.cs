928d7ab52e2d990185bd0889dd481f7c4dca031b
DESKTOP-8900FC7\Atents <silvercrowkws@naver.com>
2024-06-18 15:09:36 +0900

    수정

--- Assets/Scripts/UI/HPGaugeUI.cs
+++ Assets/Scripts/UI/HPGaugeUI.cs
@@ -12,32 +12,23 @@ public class HPGaugeUI : MonoBehaviour
     Slider slider;
 
     /// <summary>
-    /// 게임 매니저
+    /// 몬스터 베이스
     /// </summary>
-    GameManager gameManager;
-
     MonsterBase monsterBase;
 
     private void Awake()
     {
         slider = GetComponent<Slider>();
 
-        //gameManager = GameManager.Instance;
-
         monsterBase = GetComponentInParent<MonsterBase>();      // 각자 부모에서 찾음
-        monsterBase.onHPChange += OnHPChangea;
+        monsterBase.onHPChange += OnHPChange;
         slider.value = 1;
-    }
-
-    private void OnHPChangea(float currentHP)
-    {
-        slider.value = currentHP / monsterBase.maxHP;
     }
 
     /// <summary>
     /// HP가 변경된 델리게이트를 받아서 HP UI를 갱신하는 함수
     /// </summary>
-    /// <param name="obj">현재 남아있는 체력</param>
+    /// <param name="currentHP">현재 남아있는 체력</param>
     private void OnHPChange(float currentHP)
     {
         slider.value = currentHP / monsterBase.maxHP;
