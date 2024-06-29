233d92db33624648b3ab9f379d00f46452babb36
silvercrowkws <silvercrowkws@naver.com>
2024-06-29 19:08:59 +0900

    몬스터 선택 정상처리

--- Assets/Scripts/UI/MonsterNonPickButton.cs
+++ Assets/Scripts/UI/MonsterNonPickButton.cs
@@ -7,192 +7,170 @@ using UnityEngine.UI;
 public class MonsterNonPickButton : MonoBehaviour
 {
     MonsterClickButton monsterClickButton;
-    
+
+    /// <summary>
+    /// 몬스터를 픽했을 때 보여질 버튼
+    /// </summary>
     Button[] buttons;
 
     /// <summary>
-    /// 버튼이 클릭되었을 때 알파값을 변경하기 위함
+    /// 알파값을 바꾸기 위한 이미지
     /// </summary>
     Image[] images;
 
     /// <summary>
-    /// 버튼을 클릭해서 버튼의 위치를 바꿀때 사용할 리스트
-    /// </summary>
-    public List<Vector3> setPickPositionList;
-
-    /// <summary>
-    /// 버튼의 위치를 바꾸고 빠꾼 위치를 저장할 리스트
-    /// </summary>
-    public List<Vector3> pickedPositionList;
-
-    /// <summary>
-    /// 몬스터를 취소해서 알파값 바꾸라고 알리는 델리게이트
+    /// 몬스터 비활성화 시 알파값 변경을 알리는 델리게이트
     /// </summary>
     public Action<string, int> onNonPickMonster;
 
+    /// <summary>
+    /// 몬스터를 픽했을 때 버튼이 옮겨질 위치 배열
+    /// </summary>
+    public Vector3[] buttonPositions;
+
+    /// <summary>
+    /// 버튼 순서 번호 변수
+    /// </summary>
+    int buttonNumber = 0;
+
+    /// <summary>
+    /// 몬스터 이름과 버튼 위치를 저장할 딕셔너리
+    /// </summary>
+    public Dictionary<string, int> numDictionary;
+
+    /// <summary>
+    /// 비활성화된 위치를 저장할 리스트
+    /// </summary>
+    private List<int> availablePositions = new List<int>();
+
     private void Awake()
     {
-        // 버튼 배열을 자식 개수만큼 초기화
-        buttons = new Button[transform.childCount];
-
-        // 이미지 배열 초기화
-        images = new Image[transform.childCount];
+        buttons = new Button[transform.childCount];         // 버튼 배열을 자식 개수만큼 초기화
+        images = new Image[transform.childCount];           // 이미지 배열을 자식 개수만큼 초기화
 
         for (int i = 0; i < transform.childCount; i++)
         {
-            Transform child = transform.GetChild(i);
-            buttons[i] = child.GetComponent<Button>();
-            images[i] = child.GetComponent<Image>();
+            Transform child = transform.GetChild(i);        // 자식 트랜스폼 가져오기
+            buttons[i] = child.GetComponent<Button>();      // 자식 버튼 컴포넌트 가져오기
+            images[i] = child.GetComponent<Image>();        // 자식 이미지 컴포넌트 가져오기
 
-            //rectTransforms[i] = buttons[i].GetComponent<RectTransform>();
-
-
-                int index = i;
+            int index = i;                                  // 현재 인덱스를 로컬 변수에 저장
             buttons[i].onClick.AddListener(() =>
             {
-                NonPickMonster(index);
+                NonPickMonster(index);                      // 버튼 클릭 시 NonPickMonster 함수 호출
             });
         }
     }
 
     private void Start()
     {
-        monsterClickButton = FindAnyObjectByType<MonsterClickButton>();
-        monsterClickButton.onPickMonster += OnSetButtonTransform;
+        monsterClickButton = FindAnyObjectByType<MonsterClickButton>();     // MonsterClickButton 클래스 인스턴스 찾기
+        monsterClickButton.onPickMonster += OnSetButtonTransform;           // onPickMonster 델리게이트에 OnSetButtonTransform 함수 등록
 
-        // 리스트 초기화
-        setPickPositionList = new List<Vector3>();
+        buttonPositions = new Vector3[4];                                   // 버튼 위치 배열 초기화
 
-        setPickPositionList.Add(new Vector3(-475, 255, 0));
-        setPickPositionList.Add(new Vector3(-645, 150, 0));
-        setPickPositionList.Add(new Vector3(-475, 45, 0));
-        setPickPositionList.Add(new Vector3(-303, 150, 0));
+        buttonPositions[0] = new Vector3(-475, 255, 0);                     // 위쪽
+        buttonPositions[1] = new Vector3(-645, 150, 0);                     // 왼쪽
+        buttonPositions[2] = new Vector3(-475, 45, 0);                      // 아래쪽
+        buttonPositions[3] = new Vector3(-303, 150, 0);                     // 오른쪽
 
-        // 리스트 초기화
-        pickedPositionList = new List<Vector3>();
-
-        // 4개 포지션 중에서 순서대로 고른다
-        // 4개 포지션이 다 차있으면 더이상 고를 수 없다
-        // 위에 있는 버튼을 누르면 다시 active(false) 시키고 리스트에서 뺀다
-        // 이후 아래 버튼 활성화 시킨다
+        numDictionary = new Dictionary<string, int>();                      // 딕셔너리 초기화
     }
 
     /// <summary>
-    /// 몬스터를 픽해서 선택했다는 위치로 이동시킬 함수
+    /// 몬스터를 선택했을 때 버튼 위치를 설정하는 함수
     /// </summary>
-    /// <param name="name"></param>
+    /// <param name="name">몬스터 이름</param>
     private void OnSetButtonTransform(string name)
     {
+        int posIndex;
+
+        if (availablePositions.Count > 0)       // 사용 가능한 위치가 있는지 확인
+        {
+            posIndex = availablePositions[0];   // 사용 가능한 첫 번째 위치를 가져옴
+            availablePositions.RemoveAt(0);     // 사용된 위치를 리스트에서 제거
+        }
+        else
+        {
+            posIndex = buttonNumber;            // 사용 가능한 위치가 없으면 새로운 위치 할당
+            buttonNumber++;                     // 버튼 순서 번호 증가
+        }
+
         switch (name)
         {
             case "01_Water Monster":
-                buttons[0].gameObject.SetActive(true);
-                RectTransform rectTransform0 = buttons[0].GetComponent<RectTransform>();
-                //rectTransform0.anchoredPosition = new Vector2(-475, 255);     // 원하는 위치로 이동
-                rectTransform0.anchoredPosition = setPickPositionList[0];       // 이 버튼의 위치를 setPickPositionList의 0번으로 옮기고
-                pickedPositionList.Add(setPickPositionList[0]);                 // setPickPositionList 0번을 pickedPositionList로 따로 저장
-                setPickPositionList.RemoveAt(0);                                // setPickPositionList의 0번 제거
+                SetButtonPosition(0, name, posIndex);       // 물 몬스터의 위치 설정
                 break;
             case "02_Fire Monster":
-                buttons[1].gameObject.SetActive(true);
-                RectTransform rectTransform1 = buttons[1].GetComponent<RectTransform>();
-                //rectTransform0.anchoredPosition = new Vector2(-475, 255);     // 원하는 위치로 이동
-                rectTransform1.anchoredPosition = setPickPositionList[0];       // 이 버튼의 위치를 setPickPositionList의 0번으로 옮기고
-                pickedPositionList.Add(setPickPositionList[0]);                 // setPickPositionList 0번을 pickedPositionList로 따로 저장
-                setPickPositionList.RemoveAt(0);                                // setPickPositionList의 0번 제거
+                SetButtonPosition(1, name, posIndex);       // 불 몬스터의 위치 설정
                 break;
             case "03_Wind Monster":
-                buttons[2].gameObject.SetActive(true);
-                RectTransform rectTransform2 = buttons[2].GetComponent<RectTransform>();
-                //rectTransform0.anchoredPosition = new Vector2(-475, 255);     // 원하는 위치로 이동
-                rectTransform2.anchoredPosition = setPickPositionList[0];       // 이 버튼의 위치를 setPickPositionList의 0번으로 옮기고
-                pickedPositionList.Add(setPickPositionList[0]);                 // setPickPositionList 0번을 pickedPositionList로 따로 저장
-                setPickPositionList.RemoveAt(0);                                // setPickPositionList의 0번 제거
+                SetButtonPosition(2, name, posIndex);       // 바람 몬스터의 위치 설정
                 break;
             case "04_Light Monster":
-                buttons[3].gameObject.SetActive(true);
-                RectTransform rectTransform3 = buttons[3].GetComponent<RectTransform>();
-                //rectTransform0.anchoredPosition = new Vector2(-475, 255);     // 원하는 위치로 이동
-                rectTransform3.anchoredPosition = setPickPositionList[0];       // 이 버튼의 위치를 setPickPositionList의 0번으로 옮기고
-                pickedPositionList.Add(setPickPositionList[0]);                 // setPickPositionList 0번을 pickedPositionList로 따로 저장
-                setPickPositionList.RemoveAt(0);                                // setPickPositionList의 0번 제거
+                SetButtonPosition(3, name, posIndex);       // 빛 몬스터의 위치 설정
                 break;
             case "05_Dark Monster":
-                buttons[4].gameObject.SetActive(true);
-                RectTransform rectTransform4 = buttons[4].GetComponent<RectTransform>();
-                //rectTransform0.anchoredPosition = new Vector2(-475, 255);     // 원하는 위치로 이동
-                rectTransform4.anchoredPosition = setPickPositionList[0];       // 이 버튼의 위치를 setPickPositionList의 0번으로 옮기고
-                pickedPositionList.Add(setPickPositionList[0]);                 // setPickPositionList 0번을 pickedPositionList로 따로 저장
-                setPickPositionList.RemoveAt(0);                                // setPickPositionList의 0번 제거
+                SetButtonPosition(4, name, posIndex);       // 어둠 몬스터의 위치 설정
                 break;
         }
     }
 
     /// <summary>
-    /// attackGuageList에서 빼라고 알리는 함수
+    /// 버튼 위치를 설정하는 함수
     /// </summary>
-    /// <param name="index">전체 비활성화 되어 있던 버튼 중 몇번인가?</param>
-    /// <exception cref="NotImplementedException"></exception>
+    /// <param name="index">버튼 번호</param>
+    /// <param name="name">몬스터 이름</param>
+    /// <param name="posIndex">위치 번호</param>
+    private void SetButtonPosition(int index, string name, int posIndex)
+    {
+        buttons[index].gameObject.SetActive(true);                                      // 버튼 활성화
+        RectTransform rectTransform = buttons[index].GetComponent<RectTransform>();     // 픽된 몬스터의 RectTransform 컴포넌트 가져오기
+        rectTransform.anchoredPosition = buttonPositions[posIndex];                     // 버튼 위치 설정
+        numDictionary[name] = posIndex;                                                 // 딕셔너리에 몬스터 이름과 위치 저장
+    }
+
+    /// <summary>
+    /// 몬스터 비활성화 함수
+    /// </summary>
+    /// <param name="index"></param>
     private void NonPickMonster(int index)
     {
         Debug.Log($"몬스터 비활성화 : {index}");
 
+        if (!buttons[index].gameObject.activeSelf)      // 버튼이 비활성화 상태인지 확인
+        {
+            return;
+        }
+
+        string monsterName = "";
         switch (index)
         {
             case 0:
-                if (buttons[0].gameObject.activeSelf)                   // 몬스터를 픽해서 버튼이 활성화 상태이면
-                {
-                    buttons[0].gameObject.SetActive(false);
-                    onNonPickMonster?.Invoke("01_Water Monster", 0);
-                    setPickPositionList.Insert(0, pickedPositionList[pickedPositionList.Count - 1]);
-                    //setPickPositionList.Insert(0 , pickedPositionList[0]);     // 다시 setPickPositionList의 0번 위치에 pickedPositionList의 0번을 돌려줌
-                    pickedPositionList.RemoveAt(pickedPositionList.Count - 1); break;              // pickedPositionListdml 0번 제거
-                }
+                monsterName = "01_Water Monster";       // 물 몬스터 이름 설정
                 break;
             case 1:
-                if (buttons[1].gameObject.activeSelf)
-                {
-                    buttons[1].gameObject.SetActive(false);
-                    onNonPickMonster?.Invoke("02_Fire Monster", 1);
-                    setPickPositionList.Insert(0, pickedPositionList[pickedPositionList.Count - 1]);
-                    //setPickPositionList.Insert(0 , pickedPositionList[0]);     // 다시 setPickPositionList의 0번 위치에 pickedPositionList의 0번을 돌려줌
-                    pickedPositionList.RemoveAt(pickedPositionList.Count - 1); break;              // pickedPositionListdml 0번 제거
-                }
+                monsterName = "02_Fire Monster";        // 불 몬스터 이름 설정
                 break;
             case 2:
-                if (buttons[2].gameObject.activeSelf)
-                {
-                    buttons[2].gameObject.SetActive(false);
-                    onNonPickMonster?.Invoke("03_Wind Monster", 2);
-                    setPickPositionList.Insert(0, pickedPositionList[pickedPositionList.Count - 1]);
-                    //setPickPositionList.Insert(0 , pickedPositionList[0]);     // 다시 setPickPositionList의 0번 위치에 pickedPositionList의 0번을 돌려줌
-                    pickedPositionList.RemoveAt(pickedPositionList.Count - 1); break;              // pickedPositionListdml 0번 제거
-                }
+                monsterName = "03_Wind Monster";        // 바람 몬스터 이름 설정
                 break;
             case 3:
-                if (buttons[3].gameObject.activeSelf)
-                {
-                    buttons[3].gameObject.SetActive(false);
-                    onNonPickMonster?.Invoke("04_Light Monster", 3);
-                    setPickPositionList.Insert(0, pickedPositionList[pickedPositionList.Count - 1]);
-                    //setPickPositionList.Insert(0 , pickedPositionList[0]);     // 다시 setPickPositionList의 0번 위치에 pickedPositionList의 0번을 돌려줌
-                    pickedPositionList.RemoveAt(pickedPositionList.Count - 1); break;              // pickedPositionListdml 0번 제거
-                }
+                monsterName = "04_Light Monster";       // 빛 몬스터 이름 설정
                 break;
             case 4:
-                if (buttons[4].gameObject.activeSelf)
-                {
-                    buttons[4].gameObject.SetActive(false);
-                    onNonPickMonster?.Invoke("05_Dark Monster", 4);
-                    setPickPositionList.Insert(0, pickedPositionList[pickedPositionList.Count - 1]);
-                    //setPickPositionList.Insert(0 , pickedPositionList[0]);     // 다시 setPickPositionList의 0번 위치에 pickedPositionList의 0번을 돌려줌
-                    pickedPositionList.RemoveAt(pickedPositionList.Count - 1); break;              // pickedPositionListdml 0번 제거
-                }
+                monsterName = "05_Dark Monster";        // 어둠 몬스터 이름 설정
                 break;
         }
-    }
 
-    /// 들어갈 위치를 배열로 만들고 index를 0 1 2 3으로 설정한다
-    /// 픽 했을 때 인덱스를 순서대로 준다
-    /// 픽을 취소했을 때 해당 인덱스를 반환한다
+        if (numDictionary.TryGetValue(monsterName, out int posIndex))       // 딕셔너리에서 몬스터 이름으로 위치 확인
+        {
+            buttons[index].gameObject.SetActive(false);         // 버튼 비활성화
+            onNonPickMonster?.Invoke(monsterName, index);       // 델리게이트 호출
+            numDictionary.Remove(monsterName);                  // 딕셔너리에서 몬스터 이름 제거
+
+            // 비활성화된 위치를 리스트에 추가
+            availablePositions.Add(posIndex);
+        }
+    }
 }
