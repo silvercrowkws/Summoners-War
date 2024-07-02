ef77696acb81b28dde1fe143f16cb894919ebcef
DESKTOP-8900FC7\Atents <silvercrowkws@naver.com>
2024-07-02 12:08:10 +0900

    AButton을 Interactable을 해제하고 IPointer 핸들러로 변경하는 것으로 수정

--- Assets/Scripts/UI/Button/AButton.cs
+++ Assets/Scripts/UI/Button/AButton.cs
@@ -2,12 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using static GameManager;
 
-public class AButton : MonoBehaviour
+public class AButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     Button aButton;
+
+    /// <summary>
+    /// 버튼의 이미지
+    /// </summary>
+    Image image;
+
+    /// <summary>
+    /// IPointer 핸들러로 눌렸을 때, 떨어졌을 때 바뀔 이미지
+    /// </summary>
+    public Sprite[] sprites;
 
     /// <summary>
     /// A키를 눌렀다고 알리는 델리게이트
@@ -17,7 +28,8 @@ public class AButton : MonoBehaviour
     private void Awake()
     {
         aButton = GetComponent<Button>();
-        aButton.onClick.AddListener(OnAClick);
+        //aButton.onClick.AddListener(OnAClick);
+        image = aButton.GetComponent<Image>();
     }
 
     private void Start()
@@ -25,9 +37,33 @@ public class AButton : MonoBehaviour
         //aButton.gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// onClick로 작동할 함수
+    /// </summary>
     private void OnAClick()
     {
         // 공격함수와 연결 필요
+        onAClick?.Invoke();
+    }
+
+    /// <summary>
+    /// IPointer 핸들러로 눌러졌을 때
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("A 버튼 누름");
+        image.sprite = sprites[1];
+    }
+
+    /// <summary>
+    /// IPointer 핸들러로 떨어졌을 때
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("A 버튼 땜");
+        image.sprite = sprites[0];
         onAClick?.Invoke();
     }
 }
